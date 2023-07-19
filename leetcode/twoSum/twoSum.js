4cd41925a077ca72ef484ef94e1fbc02a9712588
Alice <ajlmcilraith@gmail.com>
2023-07-19 15:20:08 -0500

    complete Two Sum solution
    Complete algorithm, solution contains two nested loops and satisfies
    all test cases.
    Change test cases to use toEqual instead of toBe as the test is
    evaluating an array.

--- leetcode/twoSum/twoSum.js
+++ leetcode/twoSum/twoSum.js
@@ -1,10 +1,33 @@
-let twoSumFunction = function(numberArray, target) {
-  // Filter function to pick out the two that sum to the target
-  //? Rule out any numbers >= to the target? 
+let twoSumFunction = function (numberArray, target) {
+  
+  // Declare result 
   let result = [];
+
+  // Loop over the array twice
   for (i in numberArray) {
+
+    // If the result is populated, end the loop
+    // ! This could be a while loop
+    if (result.length >= 2 ) {
+      break;
+    }
+    // Loop again inside the initial loop
+    for (j in numberArray) {
+
+      // Check the sum against the target and rule out checking the same one twice
+      // ? Could this be a for(let j = i + 1; etc) loop? 
+      if (numberArray[i] + numberArray[j] === target && i !== j) {
+
+        // Push indices to the result array
+        result.push(+i, +j);
+
+        // Break out of the inner loop
+        break;
+      }
+    }
   }
-  return result
-}
+  // Return result array
+  return result;
+};
 
 module.exports = twoSumFunction;
